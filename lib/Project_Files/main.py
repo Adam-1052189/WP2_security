aa10fe1967558501f4eabebdc253167a6740c003
Micha-1057421 <1057421@hr.nl>
2023-11-27 18:30:05 +0100

    alles proberen te linken

--- lib/Project_Files/main.py
+++ lib/Project_Files/main.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def home():
+    return render_template("Homepage.html")
 @app.route('/login')
 def login():
     return render_template('login_page.html')
+@app.route("/notes")
+def notes():
+    return render_template('Overzicht_notities.html')
 
 
 if __name__ == "__main__":
