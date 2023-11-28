f953db4a555c32f72c746b0a0b53a69dddf1671c
unknown <1074726@hr.nl>
2023-11-28 21:52:04 +0100

    navbar toegevoegd op notitie aanmaak pagina

--- lib/Project_Files/main.py
+++ lib/Project_Files/main.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route('/login')
+@app.route("/home")
 def home():
     return render_template('homepage.html')
+@app.route("/login")
 def login():
     return render_template('login_page.html')
+@app.route("/overzicht")
+def notities():
+    return render_template('overzicht_notities.html')
+
+@app.route("/notitie")
+def notitieaanmaken():
+    return render_template('maaknotitie.html')
 
 
 if __name__ == "__main__":
