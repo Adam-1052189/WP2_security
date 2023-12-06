4ad82b2b43e1b8479b7016ddfb2696fc8f699d0a
Niels-1079140 <1079140@hr.nl>
2023-12-06 13:14:17 +0100

    Begin notitie overzicht/aanmaken

--- Project_Files/main.py
+++ Project_Files/main.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
 def home():
     return render_template('homepage.html')
 @app.route("/login", methods=['GET', 'POST'])
@@ -11,7 +10,13 @@ def login():
     return render_template('login_page.html')
 @app.route("/overzicht", methods=['GET','POST'])
 def notities():
-    return render_template('overzicht_notities.html')
+    return render_template('overzicht_notities.html', messages=messages)
+
+messages = [{'title': 'Notitie Naam',
+             'content': 'Notitie Inhoud'},
+            {'title': 'Notitie Naam',
+             'content': 'Notitie Inhoud'}
+            ]
 
 @app.route("/notitie")
 def notitieaanmaken():
