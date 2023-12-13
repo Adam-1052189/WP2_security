943cb70e6d2122b917c6ebb97db39616fab57e20
Micha-1057421 <1057421@hr.nl>
2023-12-13 12:50:12 +0100

    login werkende gemaakt met database

--- Project_Files/DB.py
+++ Project_Files/DB.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-conn = sqlite3.connect('databases/testgpt.db')
+conn = sqlite3.connect('databases/testgpt.db', check_same_thread=False)
 
-def login(username,password):
-    query= 'SELECT username, teacher_password FROM teachers WHERE username=? AND teacher_password=?;'
-    cursor= conn.execute(query, (username,password))
+def login(username, password):
+    query = 'SELECT username, teacher_password FROM teachers WHERE username=? AND teacher_password=?;'
+    cursor = conn.execute(query, (username, password))
     user = cursor.fetchone()
     return user is not None
